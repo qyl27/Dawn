09b4b1b3056ad036aed632bf49252d2708342e3e
秋雨落 <i@rain.cx>
2020-09-19 17:27:05 +0800

    Init && Craftings
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/bukkit/dawn/Dawn.java
@@ -0,0 +1,24 @@
+package cx.rain.mc.bukkit.dawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dawn extends JavaPlugin {
+    public static Dawn INSTANCE = null;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        INSTANCE = this;
+        getLogger().info("Yes, I am worked.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Goodbye!");
+    }
+
+    public static Dawn getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ src/main/java/cx/rain/mc/bukkit/dawn/crafting/DawnCrafting.java
@@ -0,0 +1,19 @@
+package cx.rain.mc.bukkit.dawn.crafting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DawnCrafting extends Craftings {
+    public DawnCrafting(JavaPlugin pluginIn) {
+        super(pluginIn);
+    }
+
+    @Override
+    public void addCraftings() {
+
+    }
+
+    @Override
+    public void removeCraftings() {
+
+    }
+}
